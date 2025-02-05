5082ed53b64fdc0e64c3e96062057d4f9d26c86f
Konrad Ciesielski <108412658+KondyKox@users.noreply.github.com>
2025-02-05 12:50:00 +0100

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,16 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TM75FS9H');</script>
+        <!-- End Google Tag Manager -->
         <link rel="icon" type="image/svg+xml" href="/logo.svg" />
         <title>Tarnowska Mafia</title>
+        <meta name="google-adsense-account" content="ca-pub-2534586073857624">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
@@ -29,9 +37,12 @@ export default function RootLayout({
           content="Tarnowska Mafia, Ekipa Totalnych Pojebów, Banda Kretynów, Tarnów Jezierny"
         />
         <meta name="author" content="Tarnowska Mafia" />
-        <meta name="google-adsense-account" content="ca-pub-2534586073857624">
       </head>
       <body className="bg-primary text-secondary">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM75FS9H"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         {/* <ThemeButton /> */}
         <Navbar />
         <main>{children}</main>
